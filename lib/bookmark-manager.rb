36b7a4975c4bfce787cb7c3b9a1101b054ba2263
Nicola Aitken <naitken22@gmail.com>
2014-07-28 16:13:03 +0100

    Add user function is created

--- lib/bookmark-manager.rb
+++ lib/bookmark-manager.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
 require 'database_cleaner'
+require './lib/user'
 require './lib/tag'
 require './lib/link'
 
@@ -15,6 +16,8 @@ DataMapper.auto_upgrade!
 class BookmarkManager < Sinatra::Base
 
 	set :views, './views'
+	enable :sessions
+	set :session_secret, 'super secret'
 
   get '/' do
     @links = Link.all 
@@ -36,6 +39,24 @@ class BookmarkManager < Sinatra::Base
   	@links = tag ? tag.links : []
   	erb :index
   end
+
+  get '/users/new' do
+  	erb :"users/new"
+  end
+
+  post '/users' do
+  	user = User.create(:email => params[:email],
+  							:password => params[:password])
+  	session[:user_id] = user.id 
+  	redirect to('/')
+  end
+
+  helpers do 
+  	def current_user
+  		@current_user ||=User.get(session[:user_id]) if session[:user_id]
+  	end
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
