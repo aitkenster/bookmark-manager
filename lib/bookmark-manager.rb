722671620b9fc5c3522d99b3e87fc7a61cd9fbe9
Nicola Aitken <naitken22@gmail.com>
2014-07-29 10:56:45 +0100

    flash error message displays

--- lib/bookmark-manager.rb
+++ lib/bookmark-manager.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
 require 'database_cleaner'
+require 'rack-flash'
 require './lib/user'
 require './lib/tag'
 require './lib/link'
@@ -18,6 +19,7 @@ class BookmarkManager < Sinatra::Base
 	set :views, './views'
 	enable :sessions
 	set :session_secret, 'super secret'
+  use Rack::Flash
 
   get '/' do
     @links = Link.all 
@@ -41,15 +43,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
-  	erb :"users/new"
+  	@user = User.new
+    erb :"users/new"
   end
 
   post '/users' do 
-    user = User.create(:email => params[:email],
+    @user = User.create(:email => params[:email],
                       :password => params[:password],
                       :password_confirmation => params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash[:notice] = "Sorry, your passwords don't match"
+      erb :"users/new"
+    end
   end
 
   helpers do 
