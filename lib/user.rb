ef6db357a22d40e62bb79713341d5b47d554319d
Nicola Aitken <naitken22@gmail.com>
2014-07-29 11:18:36 +0100

    refactoring error messages

--- lib/user.rb
+++ lib/user.rb
@@ -5,7 +5,7 @@ class User
 	include DataMapper::Resource
 
 	property :id, Serial
-	property :email, String
+	property :email, String, :unique =>true, :message => "This email is already taken"
 
 	property :password_digest, Text
 
@@ -17,7 +17,7 @@ class User
 	attr_reader :password
 	attr_accessor :password_confirmation
 
-	validates_confirmation_of :password 
+	validates_confirmation_of :password, :message => "Sorry, your passwords don't match" 
 
 end
 
